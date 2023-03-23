e50275c54d464bd12b09e6512df6df1a8e033510
Emmanuel Mayor <emayor@live.com.ar>
2023-03-23 18:12:07 -0300

    Added EmployeesService and its interface

---
+++ Services/EmployeesService/EmployeesService.cs
@@ -0,0 +1,43 @@
+﻿using AcademiaBromus.DAOs;
+using AcademiaBromus.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademiaBromus.Services.Employees
+{
+    public class EmployeesService : IEmployeesService
+    {
+        private readonly IEmployeesDAO _employeesDAO;
+
+        public EmployeesService(IEmployeesDAO dao)
+        {
+            _employeesDAO = dao;
+        }
+
+        public async Task<IEnumerable<Employee>> ReadEmployees()
+        {
+            return await _employeesDAO.SelectEmployees();
+        }
+
+        public async Task<Employee?> ReadEmployee(int id)
+        {
+            return await _employeesDAO.SelectEmployee(id);
+        }
+
+        public async Task<Employee?> UpdateEmployee(int id, Employee employee)
+        {
+            return await _employeesDAO.UpdateEmployee(id, employee);
+        }
+
+        public async Task<Employee?> CreateEmployee(Employee employee)
+        {
+            return await _employeesDAO.InsertEmployee(employee);
+        }
+
+        public async Task<Employee?> DeleteEmployee(int id)
+        {
+            return await _employeesDAO.DeleteEmployee(id);
+        }
+
+
+    }
+}
---
+++ Services/EmployeesService/IEmployeesService.cs
@@ -0,0 +1,23 @@
+﻿using AcademiaBromus.Models;
+
+namespace AcademiaBromus.Services.Employees
+{
+    public interface IEmployeesService
+    {
+        // READ
+        public Task<IEnumerable<Employee>> ReadEmployees();
+
+        // READ(id)
+        public Task<Employee?> ReadEmployee(int id);
+
+        // UPDATE
+        public Task<Employee?> UpdateEmployee(int id, Employee employee);
+
+        // CREATE
+        public Task<Employee?> CreateEmployee(Employee employee);
+
+        // DELETE
+        public Task<Employee?> DeleteEmployee(int id);
+
+    }
+}
