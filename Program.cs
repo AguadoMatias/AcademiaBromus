af7660655d737365070fc4ee3e44794a7dd82595
Emmanuel Mayor <emayor@live.com.ar>
2023-03-23 18:13:43 -0300

    Added Employees dependencies and configured controllers to ignore cyclic references

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 global using AcademiaBromus.Models;
 using AcademiaBromus.DAOs;
+using AcademiaBromus.Services.Employees;
 using AcademiaBromus.Services.ShipperService;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 
 
 
@@ -9,7 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    // Para ignorar dependencias circulares entre fields (tabla Employees)
+    .AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,6 +32,8 @@ builder.Services.AddDbContext<AcademiaBromus.Data.NorthwindContext>(
 // Define la implementacion que se debe tomar para las Interfaces 
 builder.Services.AddScoped<IShipperService, ShipperService>();
 builder.Services.AddScoped<IShipperDAO, ShipperDAO>();
+builder.Services.AddScoped<IEmployeesDAO, EmployeesDAO>();
+builder.Services.AddScoped<IEmployeesService, EmployeesService>();
 
 var app = builder.Build();
 
