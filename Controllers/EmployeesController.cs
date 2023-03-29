35497f51e8ba026de965e08dac9e3235998434a0
Emmanuel Mayor <emayor@live.com.ar>
2023-03-29 20:52:07 -0300

    Added exception handling. Methods have been renamed

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 ﻿using AcademiaBromus.Models;
 using AcademiaBromus.Services.Employees;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace AcademiaBromus.Controllers
 {
@@ -15,70 +16,99 @@ namespace AcademiaBromus.Controllers
             _employeeService = service;
         }
 
-        // GET: api/Employees
         [HttpGet]
         public async Task<IActionResult> GetEmployees()
         {
-            var employees = await _employeeService.ReadEmployees();
-            if (employees == null)
+            try
             {
-                return NotFound();
+                var employees = await _employeeService.GetEmployees();
+                if (employees == null)
+                {
+                    return NotFound();
+                }
+                return Ok(employees);
             }
-            return Ok(employees);
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
-        // GET: api/Employees/5
         [HttpGet("{id}")]
         public async Task<IActionResult> GetEmployee(int id)
         {
-            var employee = await _employeeService.ReadEmployee(id);
-            if (employee == null)
+            try
             {
-                return NotFound();
+                var employee = await _employeeService.GetEmployee(id);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                return Ok(employee);
             }
-            return Ok(employee);
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
-        // PUT: api/Employees/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        public async Task<IActionResult> SetEmployee(int id, Employee employee)
         {
-            if (id != employee.EmployeeId)
+            try
             {
-                return BadRequest();
+                if (id != employee.EmployeeId)
+                {
+                    return BadRequest();
+                }
+                var updatedEmployee = await _employeeService.SetEmployee(id, employee);
+
+                if (updatedEmployee == null)
+                {
+                    return NotFound();
+                }
+                return Ok();
             }
-
-            var updatedEmployee = await _employeeService.UpdateEmployee(id, employee);
-
-            if (updatedEmployee == null)
+            catch
             {
-                return NotFound();
+                return StatusCode(500);
             }
-            return Ok();
         }
 
-        // POST: api/Employees
         [HttpPost]
-        public async Task<IActionResult> PostEmployee(Employee employee)
+        public async Task<IActionResult> CreateEmployee(Employee employee)
         {
-            var newEmployee = await _employeeService.CreateEmployee(employee);
-            if (newEmployee == null)
+            try
             {
-                return BadRequest();
+                var newEmployee = await _employeeService.CreateEmployee(employee);
+                if (newEmployee == null)
+                {
+                    return BadRequest();
+                }
+                return Ok();
             }
-            return Ok();
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
-        // DELETE: api/Employees/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteEmployee(int id)
         {
-            var deletedEmployee = await _employeeService.DeleteEmployee(id);
-            if (deletedEmployee == null)
+            try
             {
-                return NotFound();
+                var deletedEmployee = await _employeeService.DeleteEmployee(id);
+                if (deletedEmployee == null)
+                {
+                    return NotFound();
+                }
+                return NoContent();
+            } 
+            catch 
+            {
+                return StatusCode(500);
             }
-            return NoContent();
         }
     }
 }
