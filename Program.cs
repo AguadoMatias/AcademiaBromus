48ee2f379297904fefc8df120718e6ff686f521c
Emmanuel Mayor <emayor@live.com.ar>
2023-03-29 20:54:43 -0300

    Added CORS service. Improved styling.

--- Program.cs
+++ Program.cs
@@ -7,25 +7,33 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers()
     // Para ignorar dependencias circulares entre fields (tabla Employees)
     .AddJsonOptions(options =>
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
 );
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<AcademiaBromus.Data.NorthwindContext>(
     options =>
     {
         options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindDB"));
     });
 
-// Define la implementacion que se debe tomar para las Interfaces 
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
 builder.Services.AddScoped<IShipperService, ShipperService>();
 builder.Services.AddScoped<IShipperDAO, ShipperDAO>();
 builder.Services.AddScoped<IEmployeesDAO, EmployeesDAO>();
@@ -33,13 +41,12 @@ builder.Services.AddScoped<IEmployeesService, EmployeesService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
